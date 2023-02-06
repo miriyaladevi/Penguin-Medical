cbda0dff46ed9903f3acb6695b60a680b7657943
miriyaladevi <113349770+miriyaladevi@users.noreply.github.com>
2023-02-06 14:38:11 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/miriyaladevi/Medical-Insurance.git
+cd Medical-Insurance
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
+
+git clone https://github.com/miriyaladevi/Penguin-.git
+cd Penguin-
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
